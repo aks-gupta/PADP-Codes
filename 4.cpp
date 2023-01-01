a0b9cd5bccf7a7fff87aaa0a150406cac06485dd
Akshita Gupta <72127836+aks-gupta@users.noreply.github.com>
2023-01-01 17:20:03 +0530

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,116 +1,56 @@
-#include <iostream>
-#include <math.h>
-#include <string.h>
-#include <omp.h>
-using namespace std;
-double t1,t2;
-inline long strike(bool *composite,long i,long stride,long limit)
+#include <stdio.h>
+#include <error.h>
+#include<omp.h>
+#include <gd.h>
+#include<string.h>
+int main(int argc, char **argv)
 {
-	for(;i<=limit;i+=stride)
-		composite[i]=true;
-	return i;
-}
-long Friendly(long n)
-{
-	long m = (long)sqrt((double)n);
-	long w,limit,i,count=0;
-	bool *composite = new bool[n+1];
-	memset(composite,0,n+1);
-	long *striker=new long[m];
-	long *factor=new long[m];
-	long nfactor=0;
-	t1=omp_get_wtime();
-	for(i=2;i<=m;i++)
+	FILE *fp,*fp1 = {0};
+	gdImagePtr img;
+	char *iname = NULL;
+	char *oname = NULL;
+	int color, x, y, w, h,i=0;
+	int red, green, blue,tmp,tid;
+	color = x = y = w = h = 0;
+	red = green = blue = 0;
+	if(argc != 3)
+	  error(1, 0, "Usage: ./a.out input.png output.png");
+	else
 	{
-		if(!composite[i])
-		{
-			count++;
-			striker[nfactor]=strike(composite,2*i,i,m);
-			factor[nfactor++]=i;
-		}	
+	  iname = argv[1];
+	  oname = argv[2];
 	}
-	for(w=m+1;w<=n;w+=m)
-	{
-		limit=min(w+m-1,n);
-		for(long k=0;k<nfactor;k++)
-			striker[k]=strike(composite,striker[k],factor[k],limit);
-		for(i=w;i<=limit;i++)
-			if(!composite[i])
-				count++;
-	}
-	t1=omp_get_wtime()-t1;	
-	delete[]composite;
-	delete[]striker;
-	delete[]factor;
-	return count;
-}
-long Parallel(long n)
-{
-	long m = (long)sqrt((double)n);
-	long *factor = new long[m];
-	long count=0;
-	long nfactor=0;
-	t2=omp_get_wtime();
-	#pragma omp parallel
-	{
-		bool *composite=new bool[m+1];
-		long *striker=new long[m];
-		#pragma omp single
-		{
-			memset(composite,0,m);
-			for(long i=2;i<=m;++i)
-				if(!composite[i])
-				{
-					++count;
-					strike(composite,2*i,i,m);
-					factor[nfactor++]=i;
-				}	
-				
-		} 
-		long base = -1;
-		#pragma omp for reduction (+:count) 
-		for(long w=m+1;w<=n;w+=m)
-		{	
-			memset(composite,0,m);
-			if(base!=w)
-			{
-				base=w;
-				for(long k=0;k<nfactor;++k)
-					striker[k]=(base+factor[k]-1)/factor[k]*factor[k]-base;
+	if((fp = fopen(iname, "r")) == NULL)
+	  error(1, 0, "Error - fopen(): %s", iname);
+	else
+		//img = gdImageCreateFromJpeg(fp);
+		img = gdImageCreateFromPng(fp);
+	w = gdImageSX(img);
+	h = gdImageSY(img);
+	double t = omp_get_wtime();
+	printf("%d%d",w,h);
+	#pragma omp static parallel for private(y,color,red,green,tmp)
+	for(x = 0; x < w; x++){
+		for(y = 0; y < h; y++){
+			tid=omp_get_thread_num();
+			color=gdImageGetPixel(img, x, y);
+			red   = gdImageRed(img, color);
+			green = gdImageGreen(img, color);
+			blue  = gdImageBlue(img, color);
+			tmp = (red+green+blue)/ 3;
+			red = green = blue = tmp;
+			if(tid == 0 || tid == 1 || tid == 2 || tid == 3){
+				color = gdImageColorAllocate(img, red, green, blue);
+				gdImageSetPixel(img, x, y, color);
 			}
-			long limit = min(w+m-1,n)-base;
-			for(long k=0;k<nfactor;++k)
-				striker[k]=strike(composite,striker[k],factor[k],limit)-m;
-			for(long i=0;i<=limit;++i)
-				if(!composite[i])
-					++count;
-			base+=m;				
-		}
-		delete[]composite;
-		delete[]striker;	
+	 	}
 	}
-	t2=omp_get_wtime()-t2;
-	delete[]factor;
-	return count;
-}
-int main()
-{
-	long size,count;
-	cout<<endl<<" number of threads : 4\n";
-	for(int i=2;i<=8;i++)
-	{
-		size=pow(10,i);
-		count=Friendly(size);
-		cout<<endl<<size<<"\t\t"<<t1<<"\t"<<count;
-		omp_set_num_threads(4);
-		count=Parallel(size);
-		cout<<"\t"<<t2<<"\t"<<count;
-	}
-	/* long count = Parallel(100000000);
-    cout << count << " Time : " << t2 <<endl;
-	
-	long count1 =Friendly(100000000);
-	cout << count1<< "Time :" << t1 << endl;*/	
-
+	 fp1=fopen(oname,"w");
+	gdImagePng(img, fp1);
+	fclose(fp1);
+	       t = omp_get_wtime() - t;
+	gdImageDestroy(img);
+	printf("Time taken = %g\n",t);
 	return 0;
 }
+
